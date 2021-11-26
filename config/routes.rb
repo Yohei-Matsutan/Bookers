11dc3b45ced941c26bb7539f41a20a069e57c16a
Yohei-Matsutan <takeda.yohei.sp@alumni.tsukuba.ac.jp>
2021-11-26 08:22:46 +0000

    インデックス以下の骨組み完成

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
+  get 'books' => 'books#index', as: 'books'
+
+  post 'books' => 'books#create', as: 'create'
+
+  get 'books/:id' => 'books#show', as: 'book'
+
+  get 'books/:id/edit' => 'books#edit', as: 'edit'
+
+  patch 'books/:id' => 'books#update', as: 'update'
+
+  delete 'books/:id/' => 'books#destroy', as: 'destroy'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
